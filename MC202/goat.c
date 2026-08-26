8874e85b86716a0bbf296929733b88c3f1f76b6b
Carolina Christofoletti <c269202@dac.unicamp.br>
2026-08-26 20:54:31 -0300

    Coloquei a lógica inteira dentro do goat, mas ele não está rodando

--- MC202/goat.c
+++ MC202/goat.c
@@ -1,44 +1,121 @@
 #include<stdio.h>
-#include<string.h>
 #include<stdbool.h>
 
 /*Quado percorrer a frase, vai encontrando dígitos grudados.
 Depois de encontrar todos os dígitos grudados (até cinco), 
 é preciso transformá-los de char para int. Essa função faz isso
 e retorna um inteiro.*/
-int converter_int(char v[]){
-     int tamanho = sizeof(v)/sizeof(v[0]); //Número de bytes total do vetor pelo de um único elemento
-     numero = 0;
+int converter_int(char v[], int tamanho){
+     int numero = 0;
      for(int i=0; i<tamanho; i++){
-          numero = (numero*10)+(v[i]-'0') //Tirando o 0, obtenho exatamente o número na tabela ASCII
+          numero = (numero*10)+(v[i]-'0'); //Tirando o 0, obtenho exatamente o número na tabela ASCII
      }
      return numero;
 }
 
-bool operador_in(char v[], )
+/*Verifica se o caractere é um dígito*/
+bool operador_in_num(char a){
+     char numeros[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+     for(int i=0; i<10; i++){
+          if(a==numeros[i]){
+               return true;
+          }
+     }
+     return false;
+}
+
+/*Verifica se o caractere é um caractere especial*/
+bool operador_in_esp(char a){
+     char char_especiais[] = {'!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~'};
+     for(int i=0; i<30; i++){
+          if(a==char_especiais[i]){
+               return true;
+          }
+     }
+     return false;
+}
+
+/*Verifica se o caractere é uma vogal*/
+bool operador_in_vogais(char a){
+     char vogais[] = {'a', 'e', 'i', 'o', 'u'};
+     for(int i=0; i<5; i++){
+          if(a==vogais[i]){
+               return true;
+          }
+     }
+     return false;
+}
 
 int main(void){
-     int numero, indice = 0;
+     int numero = 0, indice = 0, indice1 = 0, indice2 = 0;
      char frase[20000];
-     char numeros['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], novo_numero[5];
-     char char_especiais[]
+     char novo_numero[5], nova_palavra[1000], palavra_apoio[1000];
+     int apoio_num = 0, apoio_palavra = 0;
      char nova_frase[30000];
+     char frase_final[30000];
      while(frase[indice] != '/0'){
-          if(operador_in(char_especiais, frase[indice])==True){
-               continue;
+          if(operador_in_num(frase[indice])==true){
+               while(operador_in_num(frase[indice])==true){
+                    novo_numero[apoio_num] = frase[indice];
+                    indice++;
+                    apoio_num++;
+               }
+               numero = numero + converter_int(novo_numero, apoio_num);
+               apoio_num = 0;
+          }
+          else if(operador_in_esp(frase[indice])==true){
+               indice++;
+          }
+          else if(frase[indice]==' '){
+               nova_frase[indice1] = ' ';
+               indice1++;
+          }
+          else if(operador_in_vogais(frase[indice])==true){
+               while(frase[indice]!=' '){
+                    nova_palavra[apoio_palavra] = frase[indice];
+                    indice++;
+                    apoio_palavra++;
+               }
+               nova_palavra[apoio_palavra+1] = 'm';
+               nova_palavra[apoio_palavra+2] = 'a';
+               for(int k=1; k<=(apoio_palavra+1); k++){
+                    nova_palavra[apoio_palavra+2+k] = 'a';
+               }
+               for(int k=0; k<(2*apoio_palavra+3); k++){
+                    nova_frase[indice1] = nova_palavra[k];
+                    indice1++;
+               }
+               apoio_palavra = 0;
           }
           else{
-               /*Aqui vamos separar no caso espaço, caso dígitos,
-               caso começo de palavra com vogal, o com consoante e 
-               tal*/
+               while(frase[indice]!=' '){
+                    nova_palavra[apoio_palavra] = frase[indice];
+                    indice++;
+                    apoio_palavra++;
+               }
+               for(int k=-1; k<(apoio_palavra-1); k++){
+                    palavra_apoio[k+1] = nova_palavra[k];
+               }
+               for(int k=1; k<=apoio_palavra; k++){
+                    palavra_apoio[apoio_palavra+k] = 'a';
+               }
+               for(int k=0; k<(2*apoio_palavra+1); k++){
+                    nova_frase[indice1] = palavra_apoio[k];
+                    indice1++;
+               } 
+               apoio_palavra = 0;    
           }
-          indice++;
      }
+     indice1++;
+     nova_frase[indice1] = '/0';
      if(numero!=1){
           printf("%d goat say: ", numero);
      }
      else{
           printf("%d goats say: ", numero);
      }
-
+     while(nova_frase[indice2]!='/0'){
+          printf("%c", nova_frase[indice2]);
+          indice2++;
+     }
 }
