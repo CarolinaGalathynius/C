8f14e0e4c11c5e5242a3cf795a560d574c629536
Carolina Christofoletti <c269202@dac.unicamp.br>
2026-09-01 21:34:17 -0300

    Praticamente o nome das funções que devo começar a implementar amanhã

---
+++ MC202/contatos/contatos.c
@@ -0,0 +1,34 @@
+/*Cria um vetor chamado "contatos"
+no qual cada elemento é contém
+o nome, o endereco, o telefone e o
+aniversario de alguém.*/
+typedef struct{
+    char nome[50];
+    char endereco[100];
+    char telefone[15];
+    char aniversario[8];
+} Contato;
+
+// Função para inserir um novo contato no vetor
+void inserir(char nome[50], char endereco[100], char telefone[15], char aniversario[8], Contato contatos[1000]){
+
+}
+
+// Função para remover contatos no vetor
+void remover(char nome[50]){
+
+}
+
+// Função para buscar contatos no vetor
+void busca(char nome[50]){
+
+}
+
+// Imprime todos os contatos do vetor
+void impressao(){
+
+}
+
+void main(){
+
+}
