304879e397962c71237375de70c590f21c4f791c
Carolina Christofoletti <c269202@dac.unicamp.br>
2026-09-02 21:40:13 -0300

    .

--- MC202/contatos/contatos.c
+++ MC202/contatos/contatos.c
@@ -19,17 +19,22 @@ void inserir(char nome[50], char endereco[100], char telefone[15], char aniversa
     strcpy(contatos[tamanho].endereco, endereco);
     strcpy(contatos[tamanho].telefone, telefone);
     strcpy(contatos[tamanho].aniversario, aniversario);
-    printf("Contato para %s inserido.", nome);
+    printf("Contato para %s inserido.\n\n", nome);
 }
 
 // Função para remover contatos no vetor
 void remover(char nome[50], int tamanho, Contato contatos[1000]){
+    int contador = 0;
     for(int i = 0; i < tamanho; i++){
         if(strcmp(contatos[i].nome, nome) == 0){
+            contador++;
             for(int j = i; j < tamanho - 1; j++){
                 contatos[j] = contatos[j + 1];
+            }
+            break;
         }
     }
+    printf("Contatos de %s removidos: %d.\n\n", nome, contador);
 }
 
 // Verifica se o caractere procurado está contido no nome do contato
@@ -44,19 +49,32 @@ bool operador_in(char procurado, char nome[50]){
 
 // Função para buscar contatos no vetor
 void busca(char nome[50], int tamanho, Contato contatos[1000]){
-
+    printf("Resultado da busca:\n");
+    bool encontrado = false;
+    int numero;
+    for(int i = 0; i < tamanho; i++){
+        numero = i + 1;
+        if(operador_in(nome[0], contatos[i].nome)){
+            printf("(%d) %s\t%s\t%s\t%s\n\n", numero, contatos[i].nome, contatos[i].endereco, contatos[i].telefone, contatos[i].aniversario);
+            encontrado = true;
+        }
+    }
+    if(encontrado == false){
+        printf("Nenhum contato.\n\n");
+    }
 }
 
 // Imprime todos os contatos do vetor
 void impressao(int tamanho, Contato contatos[1000]){
     int numero;
+    printf("Listagem:\n");
     for(int i =0; i < tamanho; i++){
         numero = i + 1;
-        printf("(%d) %s %s %s %s\n", numero, contatos[i].nome, contatos[i].endereco, contatos[i].telefone, contatos[i].aniversario);
+        printf("(%d) %s\t%s\t%s\t%s\n\n", numero, contatos[i].nome, contatos[i].endereco, contatos[i].telefone, contatos[i].aniversario);
     }
 }
 
-void main(){
+int main(void){
     Contato contatos[1000];
     char nome[50], endereco[100], telefone[15], aniversario[8], operacao;
     int i = 0; 
@@ -85,4 +103,5 @@ void main(){
             break;
         }
     }
+    return 0;
 }
